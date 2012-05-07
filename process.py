5d75e3fb7b3d1334a0548c3bfba8e61915d8d118
Guillermo S <guillelmo@gmail.com>
2012-05-07 12:56:10 +0200

    1. Null split issues corrected. 2. Added verification function for time splits. 3. Added 'append' option for the process script. 4. Other minor issues

--- process.py
+++ process.py
@@ -15,7 +15,7 @@ def extract(id):
     folder = prefix + str((id / 1000) * 1000)
     fname = prefix + "%(id)04d.html" % {"id": id}
 
-    print folder +"/"+fname
+    # print folder +"/"+fname
     # return
 
     f = open(folder +"/"+fname, 'r')
@@ -30,12 +30,16 @@ def extract(id):
     times = re.search("<img src=\"graph.php.*?&t=(.*?)\">", text).group(1)
     kmsplits = times.split(",")
 
+    if city.find(',') != -1:
+        city = "\""+city+"\""
+
     res = {"city": city, "cat": cat, "time": time, "place": place, "cat_place": cat_place, "splits": kmsplits}
 
     for k,v in res.iteritems():       
 
         if k != "splits":
             res[k] = str.replace(res[k],"\353", 'e')
+            res[k] = str.replace(res[k],"&#324;", 'n')
             res[k].strip()
     
     return res
@@ -43,35 +47,53 @@ def extract(id):
 
 def process(data):
 
+    data['time'] = to_seconds(data['time'])
+    data['splits'] = proc_time_splits(data['splits'])
+    data['place'] = data['place'].split("/")[0].strip()
+    data['cat_place'] = data['cat_place'].split("/")[0].strip()    
+    return data
+
+def verify_time_splits(data_splits):
+
+    last = None
+
+    for split in data_splits:
+        if last == None:
+            last = split
+        else:
+            if last > split:
+                print "error: $last > $split "
+            
+
+
+def proc_time_splits(data_splits):
+
     splits = []
-    for lap in data['splits']:
+    for lap in data_splits:
         if(lap != ""):
             splits.append(to_seconds(lap))
         else:
             splits.append(0)
 
-    switch = {0}
     pos = 0
     for lap in splits:
         if lap == 0:
             if pos == 0:
-                splits[pos] / 2
+                if splits[pos+1]:
+                    splits[pos+1] / 2
             elif pos == 4:
                 #half marathon
-                splits[pos] = ((splits[pos-1]-splits[pos-2]) / 5) + splits[pos-1]
+                if splits[pos-1] != 0 and splits[pos-2]!=0:
+                    splits[pos] = ((splits[pos-1]-splits[pos-2]) / 5) + splits[pos-1]
             elif pos == 9:
-                splits[pos] = 2 * splits[pos-1] - splits[pos-2]
+                if splits[pos-1] != 0 and splits[pos-1]!=0:
+                    splits[pos] = 2 * splits[pos-1] - splits[pos-2]
             else:
-                splits[pos] = (splits[pos-1]+splits[pos+1]) / 2            
+                if splits[pos-1] != 0 and splits[pos+1]!=0:
+                    splits[pos] = (splits[pos-1]+splits[pos+1]) / 2            
         pos+=1
-    
 
-    data['time'] = to_seconds(data['time'])
-    data['splits'] = splits
-    data['place'] = data['place'].split("/")[0].strip()
-    data['cat_place'] = data['cat_place'].split("/")[0].strip()    
-    return data
-
+    return splits
     
 def csv_record(data):
 
@@ -101,38 +123,73 @@ def to_seconds(str_time):
 def main():
 
     import getopt
-    opts, extraparams = getopt.getopt(sys.argv[1:], "r:")
+    opts, extraparams = getopt.getopt(sys.argv[1:], "r:a")
+
+    #By default, the output file will be written from scratch
+    write_file = "w"
+    
     for k,v in opts:
         if k == '-r':
             str_range = str.split(v,",")
-            bibs = range(int(str_range[0]), int(str_range[1]))            
+
+            if 'A' <= str_range[0][0].upper() <= 'Z':
+                if str_range[0][0] == str_range[1][0]:
+                    prefix = str_range[0][0]
+                    str_range[0] = str_range[0][1:]
+                    str_range[1] = str_range[1][1:]
+                else:
+                    print "Range incorrectly defined (Prefix)"
+                    return
+            else:
+                prefix = ""
+            
+            bibs = range(int(str_range[0]), int(str_range[1]))
+
+        elif k == '-a':
+            write_file = "a"
 
     try:
         bibs
     except NameError:
         print "Bib number range not defined."
         return
+    # print prefix
 
     f = open("found.txt")
     found = f.read().split(" ")
     f.close()
     
-    f = open('rotmar.csv', 'w')
+    f = open('rotmar.csv', write_file)
 
     csv_header = "city,km5,km10,km15,km20,hm,km25,km30,km35,km40,time,cat,place,cat_place\n"
 
-    f.write(csv_header)
+    if(write_file == 'w'):
+        f.write(csv_header)
+
+    count = 0
+    incomplete = 0
     
     for bib in bibs:
-        if str(bib) in found:
-            data = extract(str(bib))
+        if prefix+str(bib) in found:            
+            data = extract(prefix+str(bib))
             proc_data = process(data)
-            rec = csv_record(proc_data)
-            f.write(rec+"\n")
-        else:
-            print str(bib)+" not in range"
-            print found[1:20]
-    
+
+            if 0 in data['splits']:
+                incomplete += 1
+            else:
+                verify_time_splits(data['splits'])
+                rec = csv_record(proc_data)
+                f.write(rec+"\n")
+                count+=1
+
+        if count % 100 == 0:
+            print str(count)+" bib numbers analyzed"
+            
+        # else:
+        #     print str(bib)+" not in the found"
+        #     print found[1:20]
+    print "Total: "+str(count)+" bib numbers correctly parsed."
+    #print "Incomplete: "+str(incomplete)+" records were found incomplete"
     
 
 
